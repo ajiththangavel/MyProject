abd6ececdd5d6573066e2f8cce7573779c1e7d37
ajiththangavel <ajith.thangavel@randstaddigital.com>
2024-04-03 15:55:38 +0530

    initial commit

---
+++ app/src/main/java/dataBase/MyEntity.kt
@@ -0,0 +1,21 @@
+package dataBase
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+class MyEntity {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id_column")
+    var myid : Int = 0
+
+    // Define column for storing name
+    @ColumnInfo(name = "name_column")
+    var myname : String = ""
+
+    // Define column for storing password
+    @ColumnInfo(name = "password_column")
+    var mypassword : String  = ""
+
+}
